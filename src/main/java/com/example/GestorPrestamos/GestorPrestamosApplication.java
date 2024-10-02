3dcc10446b2aebaf6089514542c39f201c31753f
Claudio Hernandez <claudio.hernandez.h@usach.cl>
2024-10-02 14:15:50 -0300

    Creacion Proyecto

---
+++ src/main/java/com/example/GestorPrestamos/GestorPrestamosApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestorPrestamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorPrestamosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorPrestamosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GestorPrestamos/GestorPrestamosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestorPrestamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorPrestamosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
